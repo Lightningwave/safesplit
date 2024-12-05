08ccfbae2d233db52308ad5cc635cf7f964758ba
Van Nguyen <van@Vans-MacBook-Pro.local>
2024-12-05 10:07:10 +0800

    update test data

--- create_test_users.sql
+++ create_test_users.sql
@@ -1,28 +1,33 @@
-
 DELIMITER //
 
 DROP PROCEDURE IF EXISTS create_test_users //
 CREATE PROCEDURE create_test_users()
 BEGIN
 
+  -- Hash the passwords
+  DECLARE all_pw VARCHAR(255);
+  SET all_pw = '$2a$10$b.WsKp9GR.8pcdQjxMggGeCtTL7nvuc1oW2LfZu0FrM5SLv3dhkge'; -- pw123
+
   -- End User
   INSERT INTO users (username, email, password, role)
-  VALUES ('end_user', 'end_user@example.com', 'password123', 'end_user');
+  VALUES ('end_user', 'end_user@example.com', all_pw, 'end_user');
 
   -- Premium User  
   INSERT INTO users (username, email, password, role)
-  VALUES ('premium_user', 'premium_user@example.com', 'password123', 'premium_user');
+  VALUES ('premium_user', 'premium_user@example.com', all_pw, 'premium_user');
 
   -- System Admin
   INSERT INTO users (username, email, password, role)
-  VALUES ('sys_admin', 'sys_admin@example.com', 'password123', 'sys_admin');
+  VALUES ('sys_admin', 'sys_admin@example.com', all_pw, 'sys_admin');
 
   -- Super Admin
   INSERT INTO users (username, email, password, role)
-  VALUES ('super_admin', 'super_admin@example.com', 'password123', 'super_admin');
+  VALUES ('super_admin', 'super_admin@example.com', all_pw, 'super_admin');
 
 END //
 
 DELIMITER ;
 
 CALL create_test_users();
+
+-- DELETE FROM users;
