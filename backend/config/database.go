28fd70cb3868455a4c2cfa2f77bbc0f8ba45aa50
Ryan <112879268+RavenLia@users.noreply.github.com>
2024-12-15 21:58:36 +0800

    Password change
    Changed rootpassword

--- backend/config/database.go
+++ backend/config/database.go
@@ -6,7 +6,7 @@ import (
 )
 
 func SetupDatabase() (*gorm.DB, error) {
-	dsn := "root:admin123@tcp(127.0.0.1:3306)/safesplit?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:Sucemabite123!@tcp(127.0.0.1:3306)/safesplit?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
