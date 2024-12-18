e0a9f56457277c54b04e454d21179f291b9426e1
Ryan <112879268+RavenLia@users.noreply.github.com>
2024-12-18 20:57:14 +0800

    Update Settings.js
    Added Subscription Plan into Account Details

--- frontend/src/components/EndUser/Settings.js
+++ frontend/src/components/EndUser/Settings.js
@@ -50,6 +50,7 @@ const Settings = ({ user }) => {
                     <p><strong>Name:</strong> {user.name}</p>
                     <p><strong>Email:</strong> {user.email}</p>
                     <p><strong>Joined:</strong> {user.joinedDate}</p>
+                    <p><strong>Subscription Plan:</strong> {user.subcription}</p>
                     {/* Add more user account detail fields as needed */}
                 </div>
             )}
